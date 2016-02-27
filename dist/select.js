0b67f8a0c4242b2f838d03331bf475ef253cf196
mizinho <samircomputacao@hotmail.com>
2016-02-27 16:31:01 -0300

    Update select.js

--- dist/select.js
+++ dist/select.js
@@ -1750,7 +1750,7 @@ uis.directive('uiSelectSingle', ['$timeout','$compile', function($timeout, $comp
       scope.$on('uis:close', function (event, skipFocusser) {
         $timeout(function(){
           $select.focusser.prop('disabled', false);
-          if (!skipFocusser) $select.focusser[0].focus();
+          //if (!skipFocusser) $select.focusser[0].focus();
         },0,false);
       });
 
